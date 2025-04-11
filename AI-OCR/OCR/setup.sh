753465b36edeff05f26a82d53f619c552643fa7a
kian-0 <oliverkian01@gmail.com>
2025-04-10 23:02:59 -0400

    fix: packages in setup.sh

--- AI-OCR/OCR/setup.sh
+++ AI-OCR/OCR/setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 # Install required packages
-sudo apt update
-sudo apt upgrade
+sudo apt=get update
+sudo apt-get upgrade
 sudo apt install python3.12
 sudo apt install python3.12-venv
-sudo apt-get install tesseract-ocr libtesseract-dev libleptonica-dev
-sudo apt-get install libpango1.0-dev libcairo2-dev
-sudo apt-get install libicu-dev libpango1.0-dev libcairo2-dev
-sudo apt-get install tesseract-ocr-eng
-
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
 # Creating venv
 python3 -m venv .venv
+source .venv/bin/activ
 pip install -r requirements.txt

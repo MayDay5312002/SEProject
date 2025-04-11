f5ea673e46caa8742a49556568d56e85d5cd3f5e
kian-0 <oliverkian01@gmail.com>
2025-04-10 22:55:40 -0400

    update: fixed setup.sh needs python3.12 -venv uncomment lines 14-15 to create venv

--- AI-OCR/OCR/setup.sh
+++ AI-OCR/OCR/setup.sh
@@ -4,7 +4,12 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install python3.12
+sudo apt install python3.12-venv
 sudo apt-get install tesseract-ocr libtesseract-dev libleptonica-dev
 sudo apt-get install libpango1.0-dev libcairo2-dev
 sudo apt-get install libicu-dev libpango1.0-dev libcairo2-dev
 sudo apt-get install tesseract-ocr-eng
+
+# Creating venv
+python3 -m venv .venv
+pip install -r requirements.txt
